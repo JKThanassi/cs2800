239a47a5e437be2539939d9a798a43f9d399e439
JKThanassi <josephkamingthanassi@gmail.com>
2020-09-16 23:21:33 -0400

    q11 now working

--- hw2/hw2.lisp
+++ hw2/hw2.lisp
@@ -1,3 +1,68 @@
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/base-theory" :dir :system :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/custom" :dir :system :ttags :all)
+
+;; guard-checking-on is in *protected-system-state-globals* so any
+;; changes are reverted back to what they were if you try setting this
+;; with make-event. So, in order to avoid the use of progn! and trust
+;; tags (which would not have been a big deal) in custom.lisp, I
+;; decided to add this here.
+;; 
+;; How to check (f-get-global 'guard-checking-on state)
+;; (acl2::set-guard-checking :nowarn)
+(acl2::set-guard-checking :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+; Non-events:
+;(set-guard-checking :none)
+
+(set-inhibit-warnings! "Invariant-risk" "theory")
+
+(in-package "ACL2")
+(redef+)
+(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp deferred-p))
+  state)
+
+(defun print-deferred-ttag-notes-summary (state)
+  (declare (xargs :stobjs state))
+  state)
+
+(defun notify-on-defttag (val active-book-name include-bookp state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp))
+  state)
+(redef-)
+
+(acl2::in-package "ACL2S")
+
+; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
+;$ACL2s-SMode$;ACL2s
 
 #| 
   - These commands are simplifying your interactions with ACL2s
@@ -5,7 +70,7 @@
   - Do not remove them.
 
   - To learn more about what they do, see Ch2 found on the course
-	readings page
+        readings page
 |#
 
 (set-defunc-termination-strictp nil)
@@ -82,7 +147,7 @@ association list:
 
 
 (check= (mirror '((g h (a . b) . (c . d)) . (e . f)))
-	((f . e) (((d . c) b . a) . h) . g))
+        ((f . e) (((d . c) b . a) . h) . g))
 
 ;;; 6. Define a function CONS-CELL-COUNT that counts the number of CONS
 ;;; cells (i.e. the number of pairs) in a given structure
@@ -123,7 +188,7 @@ association list:
 ;;; one.
 
 (test? (implies (and (tlp l1) (tlp l2))
-		(= (len (zip-lists l1 l2)) (min (len l1) (len l2)))))
+                (= (len (zip-lists l1 l2)) (min (len l1) (len l2)))))
 
 
 ;;; 10. Write a function WALK-SYMBOL that takes a symbol x and an
@@ -132,24 +197,39 @@ association list:
 ;;; too must be walked in s. If x has no association, then WALK-SYMBOL
 ;;; should return x. 
 
+(definec walk-symbol (sym :symbol als :alist) :all
+  (let* 
+    (
+     (ret-assoc (assoc sym als))
+     (ret-car (car ret-assoc))
+     (ret-cdr (cdr ret-assoc)))
+    (cond 
+     ((eql ret-car nil) sym)
+     ((symbolp ret-cdr)
+      (walk-symbol ret-cdr als))
+      (t ret-cdr))
+  ))
 (check= (walk-symbol 'a '((a . 5))) 5)
-(check= (walk-symbol 'a '((b . c) (a . b))) c)
+(check= (walk-symbol 'a '((b . c) (a . b))) 'c)
 (check= (walk-symbol 'a '((a . 5) (b . 6) (c . a))) 5)
 (check= (walk-symbol 'c '((a . 5) (b . (a . c)) (c . a))) 5)
-(check= (walk-symbol 'b '((a . 5) (b . ((c . a))) (c . a))) ((c . a)))
+(check= (walk-symbol 'b '((a . 5) (b . ((c . a))) (c . a))) '((c . a)))
 (check= (walk-symbol 'd '((a . 5) (b . (1 2)) (c . a) (e . c) (d . e))) 5)
-(check= (walk-symbol 'd '((a . 5) (b . 6) (c . f) (e . c) (d . e))) f)
+(check= (walk-symbol 'd '((a . 5) (b . 6) (c . f) (e . c) (d . e))) 'f)
+(check= (walk-symbol 'q '((a . 5) (b . 6) (c . f) (e . c) (d . e))) 'q)
 
 ;;; 11. Write a function UNZIP-LISTS that, given two sorted lists of nats
 ;;; without duplicates, returns a sorted list of nats without
 ;;; duplicates containing the elements of both lists. 
 
+(defdata lon (listof nat))
+
 (check= (unzip-lists '((()))) '((()) ()))
 
 (test? (implies (lopp e)
-		(equal (let ((v (unzip-lists e)))
-			 (zip-lists (car v) (cdr v)))
-		       e)))
+                (equal (let ((v (unzip-lists e)))
+                         (zip-lists (car v) (cdr v)))
+                       e)))
 
 ;;; Part II Computational complexity with static & dynamic contract checking
 
