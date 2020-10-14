c3b156c6eedc4c3e211649ae6771d82c2fa0178a
JKThanassi <josephkamingthanassi@gmail.com>
2020-10-14 18:38:21 -0400

    did num 4

--- hw5/hw5.lisp
+++ hw5/hw5.lisp
@@ -1,68 +1,3 @@
-; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
-; (Nothing to see here!  Your actual file is after this initialization code);
-(make-event
- (er-progn
-  (set-deferred-ttag-notes t state)
-  (value '(value-triple :invisible))))
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
-
-;Common base theory for all modes.
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/base-theory" :dir :system :ttags :all)
-
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/custom" :dir :system :ttags :all)
-
-;; guard-checking-on is in *protected-system-state-globals* so any
-;; changes are reverted back to what they were if you try setting this
-;; with make-event. So, in order to avoid the use of progn! and trust
-;; tags (which would not have been a big deal) in custom.lisp, I
-;; decided to add this here.
-;; 
-;; How to check (f-get-global 'guard-checking-on state)
-;; (acl2::set-guard-checking :nowarn)
-(acl2::set-guard-checking :all)
-
-;Settings common to all ACL2s modes
-(acl2s-common-settings)
-;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
-(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
-
-#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
-
-(acl2::xdoc acl2s::defunc) ; almost 3 seconds
-
-; Non-events:
-;(set-guard-checking :none)
-
-(set-inhibit-warnings! "Invariant-risk" "theory")
-
-(in-package "ACL2")
-(redef+)
-(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
-  (declare (xargs :stobjs state)
-	   (ignore val active-book-name include-bookp deferred-p))
-  state)
-
-(defun print-deferred-ttag-notes-summary (state)
-  (declare (xargs :stobjs state))
-  state)
-
-(defun notify-on-defttag (val active-book-name include-bookp state)
-  (declare (xargs :stobjs state)
-	   (ignore val active-book-name include-bookp))
-  state)
-(redef-)
-
-(acl2::in-package "ACL2S")
-
-; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
-;$ACL2s-SMode$;ACL2s
 #|
 
 HW 5
@@ -264,8 +199,7 @@ variable occurrences. For a re-refresher see Ch 4. pp 76-77.
 (check= (unique-bound-vars '(let ((x 10))
                               (let ((x x))
                                 x)))
-        '(x))#|ACL2s-ToDo-Line|#
-
+        '(x))
 
 
 #| 
@@ -326,11 +260,33 @@ EDIT. You might /try/ it, but YMMV.
 ;; 4. A revv'er-upper
 
 Conjecture 4:
-(implies (boolp (boolp (boolp x))) (boolp (boolp x)))
+(implies 
+        (boolp (boolp (boolp x))) 
+        (boolp (boolp x)))
+        
+Contract Completion
+(implies 
+        (and 
+          (boolp (boolp (boolp x))) 
+          (boolp (boolp x)))
+        (boolp (boolp x)))
 
-...
+Context:
+C1. (boolp (boolp (boolp x)))
+C2. (boolp (boolp x))
+
+Proof:
+
+(boolp (boolp x))
+= { c2 }
+true
 
 QED
+
+proof comment:
+By the contract of boolp (boolp x) is always a boolean
+By the definition of boolp (boolp (boolp x)) is always true
+The implication is always true
 
 ;; 5. Prove the following conjecture.
 
