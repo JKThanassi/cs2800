ffe997e20cf3460f66e344fed16bd86461d1ff8c
JKThanassi <josephkamingthanassi@gmail.com>
2020-10-15 19:48:40 -0400

    updating gitignore and adding hw6

--- hw6/hw6.lisp
+++ hw6/hw6.lisp
@@ -1,3 +1,68 @@
+; ****************** BEGIN INITIALIZATION FOR ACL2s MODE ****************** ;
+; (Nothing to see here!  Your actual file is after this initialization code);
+(make-event
+ (er-progn
+  (set-deferred-ttag-notes t state)
+  (value '(value-triple :invisible))))
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading the CCG book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/ccg/ccg" :uncertified-okp nil :dir :system :ttags ((:ccg)) :load-compiled-file nil);v4.0 change
+
+;Common base theory for all modes.
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s base theory book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/base-theory" :dir :system :ttags :all)
+
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/custom" :dir :system :ttags :all)
+
+;; guard-checking-on is in *protected-system-state-globals* so any
+;; changes are reverted back to what they were if you try setting this
+;; with make-event. So, in order to avoid the use of progn! and trust
+;; tags (which would not have been a big deal) in custom.lisp, I
+;; decided to add this here.
+;; 
+;; How to check (f-get-global 'guard-checking-on state)
+;; (acl2::set-guard-checking :nowarn)
+(acl2::set-guard-checking :all)
+
+;Settings common to all ACL2s modes
+(acl2s-common-settings)
+;(acl2::xdoc acl2s::defunc) ;; 3 seconds is too much time to spare -- commenting out [2015-02-01 Sun]
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem loading ACL2s customizations book.~%Please choose \"Recertify ACL2s system books\" under the ACL2s menu and retry after successful recertification.") (value :invisible))
+(include-book "acl2s/acl2s-sigs" :dir :system :ttags :all)
+
+#+acl2s-startup (er-progn (assign fmt-error-msg "Problem setting up ACL2s mode.") (value :invisible))
+
+(acl2::xdoc acl2s::defunc) ; almost 3 seconds
+
+; Non-events:
+;(set-guard-checking :none)
+
+(set-inhibit-warnings! "Invariant-risk" "theory")
+
+(in-package "ACL2")
+(redef+)
+(defun print-ttag-note (val active-book-name include-bookp deferred-p state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp deferred-p))
+  state)
+
+(defun print-deferred-ttag-notes-summary (state)
+  (declare (xargs :stobjs state))
+  state)
+
+(defun notify-on-defttag (val active-book-name include-bookp state)
+  (declare (xargs :stobjs state)
+	   (ignore val active-book-name include-bookp))
+  state)
+(redef-)
+
+(acl2::in-package "ACL2S")
+
+; ******************* END INITIALIZATION FOR ACL2s MODE ******************* ;
+;$ACL2s-SMode$;ACL2s
 #|
 
 HW 6. Admission, Measures and Totality
@@ -41,26 +106,59 @@ B. If you claim the function is not admissible, identify a condition
 |#
 
 
-(definec f1 (x :tl y :int) :nat
-  (if (endp x)
-      (1+ y)
-    (1+ (f1 (rest x) y))))
+;;(definec f1 (x :tl y :int) :nat
+;;  (if (endp x)
+ ;;     (1+ y)
+  ;;  (1+ (f1 (rest x) y))))
 
-...
+;; This function is inadmissable by the following counterexample:
+;; ((Y -399) (X '(2 1)))
 
-(definec f2 (x :tl a :int) :nat
-  (if (endp x)
-      (* a (1+ a))
-    (f2 (rest x) (1+ a))))
+;;(definec f2 (x :tl a :int) :nat
+;;  (if (endp x)
+;;      (* a (1+ a))
+;;    (f2 (rest x) (1+ a))))
 
-...
+;; By the contract of * (multiplication) we know that it is only checking that the inputs are "acl2-numbers"
+;; Thus, we can not be certian that any arbitrary arguments to that function will for sure return a natural which violates our output contract.
 
 (definec f3 (x :int l :tl) :pos
   (cond ((endp l) 1)
         ((> 0 x)  (1+ (f3 (len l) l)))
-        (t        (1+ (f3 (1- x) (rest l))))))
+        (t        (1+ (f3 (1- x) (rest l))))))#|ACL2s-ToDo-Line|#
 
-...
+
+;;(definec m (x :int l :tl) :nat
+;;  (if (< x (len l)) (1+ (len l)) (len l)))
+;;proof as follows
+
+;;(implies (and (intp x) (tlp l) (not (endp l) (< x (len l))))
+;;         (> (m x l) (m x (rest l)))
+;;
+;; Context:
+;; C1. (intp x) 
+;; C2. (tlp l) 
+;; C3. (not (endp l)) 
+;; C4. (< x (len l))
+
+;; Derived Context:
+;; D1. (equal (len l) ((+ 1 (len (cdr l))) { def len, C2, if-axioms }
+;; D2. (equal (len l) ((+ 1 (len (rest l))))) {substitute cdr for rest}
+;; D3. (equal (m x l) (+ (len l) 1)) { Def m, C4, if-axioms }
+;; D4. (equal (m x (rest l)) (+ (len rest l) 1)) { Def m, C4, if-axioms }
+;;
+;; Goal:
+;; (> (m x l) (m x (rest l)))
+;;
+;; Proof:
+;; (> (m x l) (m x (rest l))
+;; = { D3, D4 }
+;; (> (+ (len l) 1) (+ (len (rest l)) 1))
+;; = { D2 }
+;; (> (+ (+ 1 (len (rest l)) 1) (+ (len (rest l)) 1)))
+;; = { arith }
+;; (> (+ 2 (len (rest l)) (+ 1 (len (rest l)))))
+
 
 #| 
 Part II Weights and Measures
